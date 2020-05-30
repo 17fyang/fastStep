9ad5b7a3b1672cd7def47f416e04e2790a276a48
17fyang <1099640098@qq.com>
2020-05-30 23:19:45 +0800

    Initial commit

---
+++ src/main/java/com/stu/fastStep/aspect/OverallAspect.java
@@ -0,0 +1,91 @@
+package com.stu.fastStep.aspect;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.alibaba.fastjson.JSONObject;
+import com.stu.fastStep.Exception.LogicException;
+
+/*
+ * date:2019-11-8
+ * author:yf
+ * info:全局的controller异常和逻辑处理方法，
+ * 			1、获取controller返回的json，封装成restful模式。
+ * 			2、扫描所有的controller包，若controller出现异常则返回errcode：500
+ * 			3、处理用户的logicException逻辑异常
+ */
+
+@Aspect
+@Component
+public class OverallAspect {
+
+	@Before("execution(* com.stu.fastStep.controller..*(..))")
+	public void urlDeal(JoinPoint jp) {
+		ServletRequestAttributes requestAttributes =
+				(ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+		HttpServletRequest request =  requestAttributes.getRequest();
+		System.out.println(getHttp(request));
+		HttpSession session=request.getSession();
+		if(session.getAttribute("id")==null)	System.out.println("未登录");
+		try {
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+	@Around("execution(* com.stu.fastStep.controller..*(..))")
+	public Object exceptionDeal(ProceedingJoinPoint pjp) {
+		Object result=null;
+		JSONObject json=new JSONObject();
+		try {
+			Object[] args=pjp.getArgs();
+
+			result = pjp.proceed(args);
+		}catch(LogicException logicE) {
+			json.put("errcode", logicE.getErrCode());
+			json.put("message", logicE.getInfo());
+			json.put("data",new JSONObject());
+			return json.toString();
+		}catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			json.put("errcode", 500);
+			json.put("message", "未知错误!");
+			json.put("data",new JSONObject());
+			return json.toString();
+		}
+		json.put("errcode", 200);
+		json.put("message", "处理成功!");
+		json.put("data", JSONObject.parse((String)result));
+		return json.toJSONString();
+	}
+
+	private String getHttp(HttpServletRequest request) {
+		String rUrl = request.getRequestURI();//得到请求URL
+		String rMet = request.getMethod();//请求方式post或get
+
+		Enumeration enu  = request.getParameterNames();//获取请求参数名信息
+		StringBuffer sb = new StringBuffer();
+		sb.append(rUrl);
+		String key="";
+		while(enu.hasMoreElements()) {
+			key = (String)enu.nextElement();//得到参数名
+			sb.append("&"+key+"="+request.getParameter(key));//获取参数值，同时进行拼装
+		}
+		sb.append("----Method-【"+rMet+"】");
+		return sb.toString();
+	}
+}
